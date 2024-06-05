58a19ecd3b7d1b12415fe73c3456a0a95e0a54e7
Qusai alsenjalwi <qusai_sn>
2024-06-05 20:54:07 +0300

    update

--- learning_time_line/JS/6-5/dom.js
+++ learning_time_line/JS/6-5/dom.js
@@ -7,11 +7,25 @@ document.getElementById("p1").addEventListener("mouseout", function() {
 });
 
 
-function changeImage(element) {
-    document.querySelector("img").src = "images/" + element.value + ".jpg"
+ 
+  function change_flag(value) {
+    document.getElementById("img").src = value;
   }
 
-  function changeImage() {
-    var gameValue = document.getElementById("img").value;
-    document.getElementById("select-game-picture").src = "img/game_" + gameValue + ".png";
+  function change_size(value) {
+    document.getElementById("p-2").style.fontSize = value;
+  }
+  function change_family(value) {
+    document.getElementById("p-2").style.fontFamily = value;
+  }
+
+  function change_style() {
+    var p_2 = document.getElementById("p-2");
+    var italic  = document.getElementById("italic");
+    var bold  = document.getElementById("bold");
+    var underline  = document.getElementById("underline");
+  
+    p_2.style.fontStyle = italic.checked ? "italic" : "normal";
+    p_2.style.fontWeight = bold.checked ? "bold" : "normal";
+    p_2.style.textDecoration = underline.checked ? "underline" : "none";
   }
