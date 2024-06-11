699d408949a3a47d2c67baa8620c78b6275e9e3c
Qusai alsenjalwi <qusai-sn@outlook.com>
2024-06-11 21:10:21 +0300

    update

---
+++ learning_time_line/JS/6-11/task3/script.js
@@ -0,0 +1,86 @@
+document.getElementById('userForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const formData = {
+        name: document.getElementById('name').value,
+        age: document.getElementById('age').value,
+        gender: document.getElementById('gender').value,
+        birthDate: document.getElementById('birthDate').value,
+        image: document.getElementById('image').value,
+        description: document.getElementById('description').value,
+        majorUniversity: document.getElementById('majorUniversity').value,
+        siblingsNumber: document.getElementById('siblingsNumber').value,
+        siblingsDescription: document.getElementById('siblingsDescription').value,
+    };
+
+    // let name = document.getElementById('name').value;
+    // let age = document.getElementById('age').value;
+    // let gender = document.getElementById('gender').value;
+    // let birthdate = document.getElementById('birthDate').value;
+    // let image = document.getElementById('image').value;
+    // let description = document.getElementById('description').value;
+    // let majorUniversity = document.getElementById('majorUniversity').value;
+    // let siblingsNumber = document.getElementById('siblingsNumber').value;
+    // let siblingsDescription = document.getElementById('siblingsDescription').value;
+
+
+    localStorage.setItem('userdata', JSON.stringify(formData));
+    Card(formData);
+
+    
+    document.getElementById('userForm').reset();
+});
+
+
+
+function Card(data) {
+     
+    const name = document.createElement('h3');
+    name.innerHTML = data.name ;
+
+    const Age = document.createElement('p');
+    Age.innerHTML = data.age ;
+
+    const gender = document.createElement('p');
+    gender.innerHTML = data.gender ;
+
+    const birthdate = document.createElement('p');
+    birthdate.innerHTML = data.birthDate ;
+
+    const img = document.createElement('p');
+    img.innerHTML = data.image ;
+
+    const des = document.createElement('p');
+    des.innerHTML = data.description ;
+
+    const uni = document.createElement('p');
+    uni.innerHTML = data.majorUniversity ;
+
+    const siblingsNumber = document.createElement('p');
+    siblingsNumber.innerHTML = data.siblingsNumber ;
+
+    const siblingsDes = document.createElement('p');
+    siblingsDes.innerHTML = data.siblingsDescription;
+
+    let con = document.getElementById('cardsContainer');
+    
+    con.appendChild(name);
+    con.appendChild(Age);
+    con.appendChild(gender);
+    con.appendChild(birthdate);
+    con.appendChild(img);
+    con.appendChild(des);
+    con.appendChild(uni);
+    con.appendChild(siblingsNumber);
+    con.appendChild(siblingsDes);
+
+
+
+    
+}
+
+
+window.onload = function() {
+    const cards = JSON.parse(localStorage.getItem('cards')) || [];
+    cards.forEach(displayCard);
+};
