2e767fea922b5a7011901e016fc07495537b8cf1
qusai_sn <qusai_sn@outlook.com>
2024-08-26 14:53:04 +0300

    update

--- learning_time_line/API Core/8-24/task 2/task 2/Controllers/ProductsController.cs
+++ learning_time_line/API Core/8-24/task 2/task 2/Controllers/ProductsController.cs
@@ -15,12 +15,21 @@ namespace task_2.Controllers
             _context = context;
         }
 
+
+
         [HttpGet]
         public ActionResult GetAllProducts()
         {
-            var result = _context.Products.ToList();
+            var result = _context.Products.OrderByDescending(x => x.Price).ToList();
+
+
             return Ok(result);
         }
+
+
+
+
+
 
         [HttpGet("{id:int}")]
         public ActionResult GetProductById(int id)
@@ -35,6 +44,9 @@ namespace task_2.Controllers
             return Ok(result);
         }
 
+
+
+
         [HttpGet("byname")]
         public ActionResult GetProductByName(string name)
         {
@@ -47,6 +59,9 @@ namespace task_2.Controllers
 
             return Ok(result);
         }
+
+
+
 
         [HttpGet("ProductbyCategory")]
         public ActionResult GetProductByCategory(string name)
@@ -68,6 +83,10 @@ namespace task_2.Controllers
             return Ok(result);
         }
 
+
+
+
+
         [HttpGet("category/{categoryId:int}")]
         public ActionResult GetProductsByCategoryId(int categoryId)
         {
@@ -80,6 +99,10 @@ namespace task_2.Controllers
 
             return Ok(result);
         }
+
+
+
+
 
         [HttpDelete("{id:int}")]
         public ActionResult DeleteProduct(int id)
@@ -96,5 +119,126 @@ namespace task_2.Controllers
 
             return NoContent();
         }
+
+
+
+
+
+        [HttpPost]
+        public ActionResult AddProduct([FromForm] Product product)
+        {
+            if (product == null)
+            {
+                return BadRequest("Product is null");
+            }
+
+            _context.Products.Add(product);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetProductById), new { id = product.ProductId }, product);
+        }
+
+
+
+
+        [HttpPost("add Product 2")]
+        public ActionResult AddProduct2([FromBody] Product product)
+        {
+            if (product == null)
+            {
+                return BadRequest("Product is null");
+            }
+
+            _context.Products.Add(product);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetProductById), new { id = product.ProductId }, product);
+        }
+
+
+
+
+
+        [HttpPost("addProductWithDTO")]
+        public ActionResult AddProduct_DTO([FromForm] ProductDTO2 product)
+        {
+            if (product == null)
+            {
+                return BadRequest("Product is null");
+            }
+
+            var uploadfolder = Path.Combine(Directory.GetCurrentDirectory(), "Images");
+
+            if (!Directory.Exists(uploadfolder))
+            {
+                Directory.CreateDirectory(uploadfolder);
+            }
+
+            var imageFile = Path.Combine(uploadfolder, product.ProductImage.FileName);
+
+            using( var stream = new FileStream(imageFile, FileMode.Create))
+            {
+                product.ProductImage.CopyToAsync(stream);
+            }
+
+            Product new_product = new Product
+            {
+                ProductName = product.ProductName,
+                Price = product.Price,
+                Description = product.Description,
+                ProductImage = product.ProductImage.FileName,
+            };
+
+
+            _context.Products.Add(new_product);
+            _context.SaveChanges();
+
+            return Ok(new_product);
+        }
+
+
+
+
+        [HttpPut("{id:int}")]
+        public ActionResult UpdateProduct(int id, [FromBody] ProductDTO updatedProduct)
+        {
+            var existingProduct = _context.Products.Find(id);
+
+            if (existingProduct == null)
+            {
+                return NotFound("Product not found");
+            }
+
+            existingProduct.ProductName = updatedProduct.ProductName;
+            existingProduct.Description = updatedProduct.Description;
+            existingProduct.Price = updatedProduct.Price;
+            existingProduct.ProductImage = updatedProduct.ProductImage;
+
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+
+
+
+        [HttpPut("Update/{id}")]
+        public ActionResult UpdateProduct_new(int id, [FromForm] ProductDTO product_dto)
+        {
+            var record = _context.Products.Find(id);
+
+            if (record == null) {
+                return NotFound();
+            }
+            record.ProductName = product_dto.ProductName;
+            record.Description = product_dto.Description;
+            record.Price = product_dto.Price;
+            record.ProductImage = product_dto.ProductImage;
+
+            _context.SaveChanges();
+
+            return Ok(record);
+        }
+
     }
 }
--- learning_time_line/API Core/8-24/task 2/task 2/Models/Product.cs
+++ learning_time_line/API Core/8-24/task 2/task 2/Models/Product.cs
@@ -19,3 +19,31 @@ public partial class Product
 
     public virtual Category? Category { get; set; }
 }
+
+public partial class ProductDTO
+{
+ 
+    public string ProductName { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public decimal? Price { get; set; }
+
+ 
+    public string? ProductImage { get; set; }
+
+ }
+
+public partial class ProductDTO2
+{
+
+    public string ProductName { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public decimal? Price { get; set; }
+
+
+    public IFormFile ProductImage { get; set; }
+
+}
